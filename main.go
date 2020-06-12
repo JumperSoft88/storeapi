c63ae3c69a6f533ea69e0c85f34506436b82566c
thakrit.ratchakote <thakrit.ratchakote@itone.co.th>
2020-06-12 11:00:28 +0700

    new main

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,10 +12,12 @@ func h()  {
 }
 
 func main() {
+	port := os.Getenv("PORT")
 	e := echo.New()
 	e.POST("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Test..")
 	})
 
-	e.Logger.Fatal(e.Start(":1234"))
+	//e.Logger.Fatal(e.Start(":1234"))
+	e.Logger.Fatal(e.Start(":"+port))
 }
